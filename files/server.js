33e89cc1b9eaed26bc29ac902c394362ddfd90f4
Simone Barani <baranisimone@itis-molinari.eu>
2025-02-05 17:49:54 +0100

    creazione server

---
+++ files/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require('http');
+const path = require('path');
+const app = express();
+const multer  = require('multer');
+const mysql = require("mysql");
+
+const conf = JSON.parse(fs.readFileSync("conf.json"));
+const connection = mysql.createConnection(conf);
+
+
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, path.join(__dirname, "files"));
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+const upload = multer({ storage: storage}).single('file');
+
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/files", express.static(path.join(__dirname, "files")));
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        
+    console.log(req.file.filename);    
+    res.json({url: "./files/" + req.file.filename});    
+  })
+});
+
+const server = http.createServer(app);
+server.listen(5600, () => {
+  console.log("- server running");
+});
